03f67f7f6544c5799b8a1982d5e584b2d8bf88a6
Luca Marchal <lucamarchal@gmail.com>
2016-03-24 21:10:47 +0000

    dump

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ var express = require('express'),
 
 app.use(express.static(__dirname + '/client'));
 
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/client/index.html');
+});
+
+app.get('/beta', function(req, res) {
+  res.sendFile(__dirname + '/client/beta.html');
+});
+
+app.get('*', function(req, res) {
+  res.redirect('/');
 });
 
 app.listen(3000);
